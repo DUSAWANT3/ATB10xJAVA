49d3e9e93c624868f4c8c4cffe6dee29ad7c8e04
DUSAWANT3 <durgesh3230@gmail.com>
2025-01-09 11:31:58 +0530

    9th comment 01

---
+++ src/Ex001_JavaBasic/Lab01_HelloWorld.java
@@ -0,0 +1,12 @@
+package Ex001_JavaBasic;
+
+public class Lab01_HelloWorld {
+    public static void main(String[] args) {
+        //main enter-- shortcut key
+        System.out.println("Hello World");
+        // sout enter --shortcut key
+
+        //single line comment
+       //    /**/ Multiline comment
+    }
+}
