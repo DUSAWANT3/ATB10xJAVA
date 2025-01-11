ca6266c53b0e7ae3d71eb5d0dc63ebc9160cb8ba
DUSAWANT3 <durgesh3230@gmail.com>
2025-01-11 23:22:34 +0530

    Commit Lab04_Variable_p1

---
+++ src/Ex001_JavaBasic/Lab04_Variable_P1.java
@@ -0,0 +1,18 @@
+package Ex001_JavaBasic;
+
+public class Lab04_Variable_P1 {
+    public static void main(String[] args) {
+// Task 11th Jan 2025 - | printf() - Table of 9
+        int b;
+        for (int i = 1; i <=10; i++) {
+            b = 9 * i;
+            System.out.printf("9*%d=%d%n", i, b); //%n -new line
+            //System.out.println();
+        }
+        int c;
+        for (int i = 1; i <=10; i++) {
+            c = 8 * i;
+            System.out.printf("8*%d=%d%n", i, c);
+        }
+    }
+}
