2fb21668d2b948f4faca207ca88d8b8f1fb53b70
DUSAWANT3 <durgesh3230@gmail.com>
2025-01-11 10:19:36 +0530

    Commit Lab04_Variable

---
+++ src/Ex001_JavaBasic/Lab04_Variable.java
@@ -0,0 +1,22 @@
+package Ex001_JavaBasic;
+
+public class Lab04_Variable {
+    public static void main(String[] args) {
+        System.out.println("Class2 11th Jan 20225");
+
+        int a = 10;
+        //data type variable_name = variable_value
+        // int -> primitive -data type- numaric value
+        //it store the whole number-without any decimal point
+        int age = 25;
+        age = 1 + age;
+        age = 25;
+                 //age -> variable name or Identifier
+        System.out.println(age);
+
+  //byte -128 to 127 (circular)
+        byte b = 127;
+
+
+    }
+}
--- src/Ex001_JavaBasic/Main.java
+++ src/Ex001_JavaBasic/Main.java
@@ -1,3 +1,5 @@
+package Ex001_JavaBasic;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
