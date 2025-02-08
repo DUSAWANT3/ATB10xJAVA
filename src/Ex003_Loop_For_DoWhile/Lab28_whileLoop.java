8839d4b47e254cacfc02ac84eb54bd256e6362d8
DUSAWANT3 <durgesh3230@gmail.com>
2025-02-08 20:51:08 +0530

    Commit V7

---
+++ src/Ex003_Loop_For_DoWhile/Lab28_whileLoop.java
@@ -0,0 +1,16 @@
+package Ex003_Loop_For_DoWhile;
+
+public class Lab28_whileLoop {
+    public static void main(String[] args) {
+/* When you are using for or while statement then it verified the condition
+before executing the block
+for & while statement also called as Entry Controlled loop
+
+ */
+        int i= 0;
+        while(i < 10){
+            System.out.print(" " +i);
+            i++;
+        }
+    }
+}
---
+++ src/Ex003_Loop_For_DoWhile/Lab29_DoWhileLoop.java
@@ -0,0 +1,14 @@
+package Ex003_Loop_For_DoWhile;
+
+public class Lab29_DoWhileLoop {
+    public static void main(String[] args) {
+        //in do while min 1 time run the block
+        int a = 0;
+        do{
+            System.out.print(" " +a);
+            a++;
+        }while (a<10);{
+
+        }
+    }
+}
