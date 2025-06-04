2c3e0220f0a3574d0c90b6531d541329297ea836
DUSAWANT3 <durgesh3230@gmail.com>
2025-06-04 12:51:25 +0530

    0406

--- src/EX11_Java_Collection/Ex06_Interview_Q_Amazon.java
+++ src/EX11_Java_Collection/Ex06_Interview_Q_Amazon.java
@@ -14,6 +14,7 @@ public class Ex06_Interview_Q_Amazon {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
+
         // Print frequency and unique numbers
         for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
             int number = entry.getKey();
