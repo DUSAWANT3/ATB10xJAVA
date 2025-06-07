b0e69859ab258d6afa1569bd052dfddf23b27dda
DUSAWANT3 <durgesh3230@gmail.com>
2025-06-08 00:15:54 +0530

    0608

--- src/EX11_Java_Collection/Ex02_LinkedList.java
+++ src/EX11_Java_Collection/Ex02_LinkedList.java
@@ -26,7 +26,6 @@ public class Ex02_LinkedList {
 // This won't compile:
 // list.addFirst("Z");
 
-
 // This will work:
         LinkedList<String> linkedList = new LinkedList<>();
         linkedList.addFirst("Z");
